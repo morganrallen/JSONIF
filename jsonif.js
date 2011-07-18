a960fec0fdf97be25b84a4556d0c8f55b6e3727a
Morgan "ARR!" Allen <morganrallen@gmail.com>
2011-07-17 17:00:01 -0700

    yeah.. did this a while ago, not sure whats going on

--- jsonif.js
+++ jsonif.js
@@ -9,72 +9,64 @@ function JSONIF(data, canvas) {
 	if(typeof data[0] !== "object")
 		throw new Error("Image data must contain header");
 
-	this.header = data[0];
-	this.data = data[1];
-	var	c = canvas ? canvas : document.createElement("canvas"),
+	var header = data[0],
+		data = data[1],
+		c = canvas ? canvas : document.createElement("canvas"),
 		ctx = c.getContext("2d");
-	this.ctx = ctx;
+		console.log(c);
 
-	// c.height = c.style.height = header.height;
-	// c.width = c.style.width = header.width;
+	c.height = c.style.height = header.height;
+	c.width = c.style.width = header.width;
 
 	var pos = { x: 0, y: 0 },
-		map = cart(this.data, this.header.width),
+		map = cart(data, header.width),
 		borderMap = [],
 		inBorder = false;
 
-}
+	ctx.save();
+	for(var y = 0; y < map.length; y++) {
+		for(var x = 0; x < map[x].length; x++) {
+			var px = map[y][x];
 
-JSONIF.prototype = {
-	draw: function(ctx) {
+			if(!borderMap[y])
+				borderMap[y] = [];
 
-		var pos = { x: 0, y: 0 },
-			map = cart(this.data, this.header.width),
-			borderMap = [],
-			inBorder = false;
-
-		for(var y = 0; y < map.length; y++) {
-			for(var x = 0; x < map[x].length; x++) {
-				var px = map[y][x];
-
-				if(!borderMap[y])
-					borderMap[y] = [];
-
-				if(px === 0) {
-					if(inBorder) {
-						borderMap[y][x - 1] = 1;
-						borderMap[y][x] = 0;
-					} else {
-						borderMap[y][x] = 0;
-					}
-
-					if(inBorder)
-						inBorder = !inBorder;
-				} else if(inBorder && borderMap[y][x] && (borderMap[y-1][x] === 0 || map[y+1][x] === 0)) {
-					borderMap[y][x] = 1;
+			if(px === 0) {
+				if(inBorder) {
+					borderMap[y][x - 1] = 1;
+					borderMap[y][x] = 0;
 				} else {
-					borderMap[y][x] = inBorder ? 2 : 1;
-					inBorder = true;
+					borderMap[y][x] = 0;
 				}
 
-				ctx.fillStyle = typeof this.header[px] == "function" ? this.header[px]() : this.header[px] || 'white';
-				ctx.fillRect(x, y, 1,1);
-
-				/* if(borderMap[y][x] == 1) { */
-				/* 	ctx.fillStyle = 'green'; */
-				/* 	ctx.fillRect(x, y, 1, 1); */
-				/* } */
-
-				/* if(borderMap[y][x-1] == 1) { */
-				/* 	ctx.fillStyle = 'green'; */
-				/* 	ctx.fillRect(x-1, y, 1, 1); */
-				/* } */
-
-				/* ct	x.fillStyle = 'red'; */
-				/* ctx.fillRect(header.center.x, header.center.y, 1,1); */
+				if(inBorder)
+					inBorder = !inBorder;
+			} else if(inBorder && borderMap[y][x] && (borderMap[y-1][x] === 0 || map[y+1][x] === 0)) {
+				borderMap[y][x] = 1;
+			} else {
+				borderMap[y][x] = inBorder ? 2 : 1;
+				inBorder = true;
 			}
+
+			ctx.fillStyle = typeof header[px] == "function" ? header[px](x, y) : header[px] || 'white';
+			ctx.fillRect(x, y, 1,1);
+
+			/* if(borderMap[y][x] == 1) { */
+			/* 	ctx.fillStyle = 'green'; */
+			/* 	ctx.fillRect(x, y, 1, 1); */
+			/* } */
+
+			/* if(borderMap[y][x-1] == 1) { */
+			/* 	ctx.fillStyle = 'green'; */
+			/* 	ctx.fillRect(x-1, y, 1, 1); */
+			/* } */
+
+			/* ctx.fillStyle = 'red'; */
+			/* ctx.fillRect(header.center.x, header.center.y, 1,1); */
 		}
-		c.borderMap = borderMap;
-		return c;
 	}
-};
+	ctx.restore();
+	c.borderMap = borderMap;
+	return c;
+}
+
