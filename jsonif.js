94866bae6bf798a42d59c3d212890baf5b20eeea
Morgan "ARR!" Allen <morganrallen@gmail.com>
2011-07-20 19:46:06 -0700

    scaler

--- jsonif.js
+++ jsonif.js
@@ -5,67 +5,38 @@ function cart(d, w) {
 	return r;
 }
 
-function JSONIF(data, canvas) {
+function JSONIF(data, canvas, config) {
 	if(typeof data[0] !== "object")
 		throw new Error("Image data must contain header");
 
+	config = config || {};
+
 	var header = data[0],
 		data = data[1],
-		c = canvas ? canvas : document.createElement("canvas"),
-		ctx = c.getContext("2d");
 
-	c.height = c.style.height = header.height;
-	c.width = c.style.width = header.width;
+		c = canvas ? canvas : document.createElement("canvas"),
+		ctx = this.ctx = c.getContext("2d"),
+		
+		scaler = this.scaler = config.scaler || 1;
+
+	c.height = c.style.height = header.height * scaler;
+	c.width = c.style.width = header.width * scaler;
 
 	var pos = { x: 0, y: 0 },
 		map = cart(data, header.width),
-		borderMap = [],
 		inBorder = false;
 
 	ctx.save();
+
 	for(var y = 0; y < map.length; y++) {
 		for(var x = 0; x < map[x].length; x++) {
 			var px = map[y][x];
 
-			if(!borderMap[y])
-				borderMap[y] = [];
-
-			if(px === 0) {
-				if(inBorder) {
-					borderMap[y][x - 1] = 1;
-					borderMap[y][x] = 0;
-				} else {
-					borderMap[y][x] = 0;
-				}
-
-				if(inBorder)
-					inBorder = !inBorder;
-			} else if(inBorder && borderMap[y][x] && (borderMap[y-1][x] === 0 || map[y+1][x] === 0)) {
-				borderMap[y][x] = 1;
-			} else {
-				borderMap[y][x] = inBorder ? 2 : 1;
-				inBorder = true;
-			}
-
-			ctx.fillStyle = typeof header[px] == "function" ? header[px](x, y) : header[px] || 'white';
-			ctx.fillRect(x, y, 1,1);
-
-			/* if(borderMap[y][x] == 1) { */
-			/* 	ctx.fillStyle = 'green'; */
-			/* 	ctx.fillRect(x, y, 1, 1); */
-			/* } */
-
-			/* if(borderMap[y][x-1] == 1) { */
-			/* 	ctx.fillStyle = 'green'; */
-			/* 	ctx.fillRect(x-1, y, 1, 1); */
-			/* } */
-
-			/* ctx.fillStyle = 'red'; */
-			/* ctx.fillRect(header.center.x, header.center.y, 1,1); */
+			ctx.fillStyle = typeof header[px] == "function" ? header[px](x, y, this) : header[px] || 'white';
+			ctx.fillRect(x*scaler, y*scaler, scaler, scaler);
 		}
 	}
 	ctx.restore();
-	c.borderMap = borderMap;
 	return c;
 }
 
